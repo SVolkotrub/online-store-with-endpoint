e3b6e7363e8e837f048d5e1f3b3ded24f1be780f
SVolkotrub <volkotrub.design@gmail.com>
2022-08-13 00:14:36 +0300

    update endpoint

--- online-store/src/pages/ProductPage.js
+++ online-store/src/pages/ProductPage.js
@@ -148,7 +148,7 @@ class ProductPage extends PureComponent {
                                                 
                                                 return (
                                                     <div key={`${index}_${item}`} className={this.state[attributeNameIndex] === index ? "attribute-item-selected" : "attribute-item"} 
-                                                        onClick={() => { this.setState({  [attributeNameIndex]: index }) }}><p key={`${index}_itemvalue`}>{item.value}</p></div>
+                                                        onClick={() => { this.setState({  [attributeNameIndex]: index }) }}><p key={`${index}_itemValue`}>{item.value}</p></div>
                                                 );
                                             })}
                                         </div>
